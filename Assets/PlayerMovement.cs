0cdcbb95026b5c528cdbacee48d995143894a2be
melasli486 <aslialtg@hotmail.com>
2026-04-24 15:39:20 +0300

    bordo karakter labirent güncellemesi

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,13 +3,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 8f;
+
+    [SerializeField] private GameObject explosionEffect;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (rb == null)
+        {
+            Debug.LogError("Lütfen Capsule nesnesine Rigidbody bileþeni ekleyin!");
+        }
+    }
 
     void Update()
     {
         float moveX = 0f;
         float moveZ = 0f;
 
-        // Klasik GetKey kullanarak Input System çakýþmasýný baypas ediyoruz
         if (Input.GetKey(KeyCode.W)) moveZ = 1f;
         if (Input.GetKey(KeyCode.S)) moveZ = -1f;
         if (Input.GetKey(KeyCode.A)) moveX = -1f;
@@ -18,8 +33,37 @@ public class PlayerMovement : MonoBehaviour
         Vector3 direction = new Vector3(moveX, 0f, moveZ).normalized;
         transform.Translate(direction * moveSpeed * Time.deltaTime);
 
-        // Mekanik: Shift ile Dash
         if (Input.GetKeyDown(KeyCode.LeftShift)) moveSpeed = 15f;
         if (Input.GetKeyUp(KeyCode.LeftShift)) moveSpeed = 5f;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Cylinder"))
+        {
+
+            if (explosionEffect != null)
+            {
+                Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            }
+
+            Debug.Log("Sütuna çarptýn! GÜÜÜM!");
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
     }
 }
